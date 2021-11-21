03ef55ed3b5f08944e60b529a4bc25e636bb9d64
Link <50463727+link-discord@users.noreply.github.com>
2021-11-21 10:17:37 +0100

    Fix Issue
    Condition 'Array.isArray(args)' is always true because variable 'args' is defined by the rest pattern at line 11.

--- src/handlers/events.ts
+++ src/handlers/events.ts
@@ -9,7 +9,7 @@ export async function eventHandler(client: BotClient): Promise<void> {
             const event = new BotEvent(client)
 
             client[event.data.once ? 'once' : 'on'](event.data.name, (...args: any[]): void => {
-                if (Array.isArray(args) && args.length === 1) args = args[0]
+                if (args.length === 1) args = args[0]
                 event.execute(args)
             })
         })
