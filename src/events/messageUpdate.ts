a2ff623f4ff86c82eca3b2ce4f7f4d7a069200a9
Link <50463727+link-discord@users.noreply.github.com>
2021-11-21 22:01:35 +0100

    refactor

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import { inspect } from '../handlers/message'
 import { BotEvent } from '../classes/index'
 import type { GuildTextBasedChannel, Message } from 'discord.js'
 import type { BotClient } from '../classes/index'
@@ -19,49 +19,6 @@ export default class MessageUpdateEvent extends BotEvent {
         if (message.author?.bot || message.webhookId != undefined || !bot.permissionsIn(channel).has('MANAGE_MESSAGES'))
             return
 
-        const whitelist = await this.client.database.whitelistedDomain.findMany({
-            where: { guild: { id: message.guildId } }
-        })
-
-        let isMalicious: boolean = false
-        let isWhiteListed: boolean = false
-
-        if (whitelist !== undefined && whitelist.length > 0) {
-            const domains = message.content.match(
-                /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/
-            )
-
-            const whitelistedDomains: string[] = whitelist.map((d) => d.domain)
-
-            // check if all links are in the whitelist
-            if (domains.every((v: string): boolean => whitelistedDomains.includes(v))) isWhiteListed = true
-        }
-
-        if (isWhiteListed) return
-
-        try {
-            const { data } = (await axios.post(
-                'https://anti-fish.bitflow.dev/check',
-                {
-                    message: message.content
-                },
-                {
-                    headers: {
-                        'User-Agent': 'Inspector (https://github.com/link-discord/inspector)'
-                    }
-                }
-            )) as any
-
-            data.matches.forEach((match: { type: string }): void => {
-                const type: string = match.type.toLowerCase()
-
-                if ((type === 'phishing' || type === 'ip_logger') && !isMalicious) isMalicious = true
-            })
-
-            if (isMalicious) await message.delete()
-        } catch (error) {
-            // @ts-ignore unknown type
-            if (!error.isAxiosError) this.client.logger.error(e)
-        }
+        await inspect(this.client, message)
     }
 }
---
+++ src/handlers/message.ts
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import { Message } from 'discord.js'
+import { BotClient } from '../classes'
+
+export async function inspect(client: BotClient, message: Message) {
+    const whitelist = await client.database.whitelistedDomain.findMany({
+        where: { guild: { id: message.guildId } }
+    })
+
+    let isMalicious: boolean = false
+    let isWhiteListed: boolean = false
+
+    if (whitelist !== undefined && whitelist.length > 0) {
+        const domains = message.content.match(
+            /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/
+        )
+
+        const whitelistedDomains: string[] = whitelist.map((d) => d.domain)
+
+        // check if all links are in the whitelist
+        if (domains.every((v: string): boolean => whitelistedDomains.includes(v))) isWhiteListed = true
+    }
+
+    if (isWhiteListed) return
+
+    try {
+        const { data } = (await axios.post(
+            'https://anti-fish.bitflow.dev/check',
+            {
+                message: message.content
+            },
+            {
+                headers: {
+                    'User-Agent': 'Inspector (https://github.com/link-discord/inspector)'
+                }
+            }
+        )) as any
+
+        data.matches.forEach((match: { type: string }): void => {
+            const type: string = match.type.toLowerCase()
+
+            if ((type === 'phishing' || type === 'ip_logger') && !isMalicious) isMalicious = true
+        })
+
+        if (isMalicious) await message.delete()
+    } catch (error) {
+        // @ts-ignore unknown type
+        if (!error.isAxiosError) this.client.logger.error(e)
+    }
+}
