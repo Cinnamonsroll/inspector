30930045cb02550f1fd9dd585da95cb2b0be4c9b
Link <50463727+link-discord@users.noreply.github.com>
2021-11-21 11:32:00 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,12 +2,18 @@
 
 # Only use this script for replit
 
-if "$(node -v)" =~ "16."
-then
-    echo "Using latest nodejs version"
-else
-    echo "Installing latest nodejs version..."
-    npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
-fi
-    echo "Starting..."
-    npx prisma db pull && npx prisma generate && npx tsc && cd dist && clear && ../node_modules/.bin/node index.js
+echo "Installing latest nodejs version..."
+
+npm init -y 
+npm i --save-dev node@16 
+npm config set prefix=$(pwd)/node_modules/node 
+export PATH=$(pwd)/node_modules/node/bin:$PATH
+
+echo "Starting..."
+
+npx prisma db pull 
+npx prisma generate 
+npx tsc 
+cd dist 
+clear 
+../node_modules/.bin/node index.js
